c117c699914a5856c00b1fc1dcaff6ece941f0ac
Тимур <namat9@yandex.ru>
2022-12-03 17:08:53 +0700

    JS

---
+++ JavaScript/Arrays/Arrays.js
@@ -0,0 +1,46 @@
+(function () {
+    var array = [10, 123.2123, 3, -2, 0, 2, 35.6, 789, 23, -85, 6];
+
+    array.sort(function (e1, e2) {
+        return e2 - e1;
+    });
+
+    console.log(array)
+    console.log(array.slice(0, 5));
+    console.log(array.slice(array.length - 5));
+
+    console.log(array.reduce(function (accumulator, currentValue) {
+        if (currentValue % 2 === 0) {
+            return accumulator + currentValue;
+        } else {
+            return accumulator;
+        }
+    }, 0));
+}());
+
+(function () {
+    var array = [];
+
+    for (var i = 1; i < 100; i++) {
+        array.push(i);
+    }
+
+    console.log(array)
+
+    var squaresEvenNumbersArray = array.filter(function (currentValue) {
+        if (currentValue % 2 === 0) {
+            return currentValue;
+        }
+    }).map(value => value * value);
+
+    /* var squaresEvenNumbersArray = array.filter(function (currentValue) {
+        if (currentValue % 2 === 0) {
+            return currentValue * currentValue;
+        }
+    });
+
+    Почему не получается вот так?
+    */
+
+    console.log(squaresEvenNumbersArray)
+})();
