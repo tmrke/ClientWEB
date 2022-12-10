49f5c17de7e3973e1b5d3f62a876f04f56d0f4d6
Тимур <namat9@yandex.ru>
2022-12-11 00:30:17 +0700

    JS Arrays

--- JS/Arrays/Arrays.js
+++ JS/Arrays/Arrays.js
@@ -1,48 +1,59 @@
-(function () {
-    var array = [10, 123.2123, 3, -2, 0, 2, 35.6, 789, 23, -85, 6];
-
+function getFirstFiveElementsArray(array) {
     array.sort(function (e1, e2) {
         return e2 - e1;
     });
 
-    console.log(array);
-    console.log(array.slice(0, 5));
-    console.log(array.slice(array.length - 5));
+    return array.slice(0, 5);
+}
 
-    console.log(array.reduce(function (evenNumbersSum, currentValue) {
-        if (currentValue % 2 === 0) {
-            return evenNumbersSum + currentValue;
+function getLastFiveElementsArray(array) {
+    array.sort(function (e1, e2) {
+        return e2 - e1;
+    });
+
+    return array.slice(array.length - 5);
+}
+
+function getEvenNumbersSumArray(array) {
+    return array.reduce(function (evenNumbersSum, currentNumber) {
+        if (currentNumber % 2 === 0) {
+            return evenNumbersSum + currentNumber;
         } else {
             return evenNumbersSum;
         }
-    }, 0));
-}());
+    }, 0);
+}
+
+function getEvenNumbersSquaresArray(array) {
+    return array
+        .filter(function (value) {
+            return value % 2 === 0
+        })
+        .map(function (value) {
+            return value * value;
+        });
+}
 
 (function () {
-    var array = [];
+    var array1 = [10, 123.2123, 3, -2, 0, 2, 35.6, 789, 23, -85, 6];
 
-    for (var i = 1; i < 100; i++) {
-        array.push(i);
+    var firstFiveElementsArray = getFirstFiveElementsArray(array1);
+    console.log("Первые пять элементов массива: {" + firstFiveElementsArray.join(", ") + "}");
+
+    var lastFiveElementsArray = getLastFiveElementsArray(array1);
+    console.log("Последние пять элементов массива: {" + lastFiveElementsArray.join(", ") + "}");
+
+    var evenNumbersSumArray = getEvenNumbersSumArray(array1);
+    console.log("Сумма четных чисел массива = " + evenNumbersSumArray);
+
+    var array2 = [];
+
+    for (var i = 1; i <= 100; i++) {
+        array2.push(i);
     }
 
-    console.log(array);
+    console.log("Массив чисел от 1 до 100: {" + array2.join(", ") + "}");
 
-    var squaresEvenNumbersArray = array.filter(function (value) {
-        if (value % 2 === 0) {
-            return value;
-        }
-    }).map(function (value) {
-        return value * value;
-    });
-
-    /* var squaresEvenNumbersArray = array.filter(function (currentValue) {
-        if (currentValue % 2 === 0) {
-            return currentValue * currentValue;
-        }
-    });
-
-    Почему не получается вот так?
-    */
-
-    console.log(squaresEvenNumbersArray);
+    var evenNumbersSquaresArray = getEvenNumbersSquaresArray(array2);
+    console.log("Список квадратов четных чисел массива: {" + evenNumbersSquaresArray.join(", ") + "}");
 })();
