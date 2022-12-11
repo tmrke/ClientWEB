7b06a505eff49335349b8b9d9839b5a15fd24e24
Тимур <namat9@yandex.ru>
2022-12-11 18:11:23 +0700

    JS Objects

--- JS/Objects/Objects.js
+++ JS/Objects/Objects.js
@@ -1,97 +1,70 @@
 (function () {
-    var russiaCountry = {};
-    var usaCountry = {};
-    var mexicoCountry = {};
-
-    var moscowCity = {
-        name: "Moscow",
-        population: 13010112
-    };
-
-    var vladivostokCity = {
-        name: "Vladivostok",
-        population: 600871
-    };
-
-    var sochiCity = {
-        name: "Sochi",
-        population: 466078
-    };
-
-    var novosibirskCity = {
-        name: "Novosibirsk",
-        population: 1633595
-    };
-
-    russiaCountry.name = "Russia";
-    russiaCountry.cities = [moscowCity, vladivostokCity, sochiCity, novosibirskCity];
-
-    var newYorkCity = {
-        name: "New York",
-        population: 8467513
-    };
-
-    var bostonCity = {
-        name: "Vladivostok",
-        population: 675647
-    };
-
-    var losAngelesCity = {
-        name: "Los Angeles",
-        population: 3898747
-    };
-
-    usaCountry.name = "USA";
-    usaCountry.cities = [newYorkCity, bostonCity, losAngelesCity];
-
-    var mexicoCity = {
+    var countries = [{
+        name: "Russia",
+        cities: [{
+            name: "Moscow", population: 13010112
+        }, {
+            name: "Vladivostok", population: 600871
+        }, {
+            name: "Sochi", population: 466078
+        }, {
+            name: "Novosibirsk", population: 1633595
+        }]
+    }, {
+        name: "USA",
+        cities: [{
+            name: "New York", population: 8467513
+        }, {
+            name: "Los Angeles", population: 3898747
+        }, {
+            name: "Vladivostok", population: 675647
+        }]
+    }, {
         name: "Mexico",
-        population: 9100000
-    };
+        cities: [{
+            name: "Mexico", population: 9100000
+        }, {
+            name: "Tijuana", population: 1964788
+        }]
+    }];
 
-    var tijuanaCity = {
-        name: "Tijuana",
-        population: 1964788
-    };
+    var citiesMaxCountsCountry = getMaxCitiesCountsCountry(countries);
+    console.log(citiesMaxCountsCountry);
 
-    mexicoCountry.name = "Mexico";
-    mexicoCountry.cities = [mexicoCity, tijuanaCity];
+    var populationsByCountry = getPopulationsByCountry(countries);
+    console.log(populationsByCountry);
+})();
 
-    var countries = [russiaCountry, usaCountry, mexicoCountry];
+function getMaxCitiesCountsCountry(countries) {
+    if (countries.length === 0) {
+        return null;
+    }
 
     countries.sort(function (country1, country2) {
         return country2.cities.length - country1.cities.length;
     });
 
-    var maxCountCitiesCountry = countries.filter(function (country) {
+    return countries.filter(function (country) {
         return country.cities.length === countries[0].cities.length;
     });
+}
 
-    console.log(maxCountCitiesCountry);
+function getPopulationsByCountry(countries) {
+    var populationsByCountry = {
+        name: [],
+        population: []
+    };
 
-    var populationsByCountry = new Map();
+    countries.forEach(function (country) {
+        var name = country.name;
+        var population = country.cities.reduce(function (sumPopulation, city) {
+            return sumPopulation + city.population;
+        }, 0)
 
-    populationsByCountry.set(russiaCountry.name, russiaCountry.cities.reduce(function (sumPopulation, city) {
-        var currentPopulation = city.population;
-        return sumPopulation + currentPopulation;
-    }, 0));
+        populationsByCountry.name.push(name);
+        populationsByCountry.population.push(population);
+    })
 
-    populationsByCountry.set(usaCountry.name, usaCountry.cities.reduce(function (sumPopulation, city) {
-        var currentPopulation = city.population;
-        return sumPopulation + currentPopulation;
-    }, 0));
+    return populationsByCountry;
+}
 
-    populationsByCountry.set(mexicoCountry.name, mexicoCountry.cities.reduce(function (sumPopulation, city) {
-        var currentPopulation = city.population;
-        return sumPopulation + currentPopulation;
-    }, 0));
-
-
-    /* похожий вопрос: почему нельзя сделать вот так?:
-     populationsByCountry.set(mexicoCountry.name, mexicoCountry.cities.population.reduce(function (sumPopulation, currentPopulation) {
-         return sumPopulation + currentPopulation;
-     }, 0));
-      */
-
-    console.log(populationsByCountry);
-})();
