284c9f99f3d6f49b083ea0885ee304173c864cd4
Тимур <namat9@yandex.ru>
2022-12-17 16:02:46 +0700

    TODO list

--- JQuery/TODO-List/todo_list.js
+++ JQuery/TODO-List/todo_list.js
@@ -3,6 +3,7 @@ $(function () {
     var addButton = $("#add-button");
     var todoList = $("#list");
     var form = $("#form");
+    var errorMessage = $("#error-message");
 
     form.submit(function (e) {
         e.preventDefault();
@@ -10,8 +11,11 @@ $(function () {
 
     addButton.click(function () {
         var newTodoListNoteText = textInput.val().trim();
+        errorMessageVisible(false);
 
         if (newTodoListNoteText.length === 0) {
+            errorMessageVisible(true);
+
             return;
         }
 
@@ -20,12 +24,22 @@ $(function () {
 
         setViewMode();
 
+        function errorMessageVisible(visible) {
+            if (visible === true) {
+                textInput.addClass("invalid");
+                errorMessage.removeClass("error-message-hidden");
+                errorMessage.addClass("error-message-visible");
+            } else {
+                errorMessage.removeClass("error-message-visible");
+                errorMessage.addClass("error-message-hidden");
+                textInput.removeClass("invalid");
+            }
+        }
+
         function setViewMode() {
-            newTodoListNote.html("<span id='span'></span>\<" +
-                "button class='edit-button' id='edit-button'>\<" +
-                "img src='/JS-DOM/TODO-List/resources/edit.png' alt='edit'>\<" +
-                "button class='delete-button' id='delete-button'>\<" +
-                "img src='/JS-DOM/TODO-List/resources/delete.png' alt='edit'>");
+            newTodoListNote.html("<span id='span'></span>" +
+                "<button class='edit-button' id='edit-button' title='edit'><img src='/JS-DOM/TODO-List/resources/edit.png' alt='edit'>" +
+                "<button class='delete-button' id='delete-button' title='delete'><img src='/JS-DOM/TODO-List/resources/delete.png' alt='delete'>");
 
             newTodoListNote.find("#span").text(newTodoListNoteText);
             textInput.val("");
@@ -40,11 +54,12 @@ $(function () {
         }
 
         function setEditMode() {
-            newTodoListNote.html("<input type='text' class='edit-input'>\
-            <button class='save-button' id='save-button'>\
+            newTodoListNote.html("<div><input type='text' class='edit-input'>\
+                <button class='save-button' id='save-button' title='save'>\
                 <img src='/JS-DOM/TODO-List/resources/save.png' alt='save'>\
-            <button class='cancel-button' id='cancel-button'>\
-                <img src='/JS-DOM/TODO-List/resources/cancel.png' alt='cancel'>");
+                <button class='cancel-button' id='cancel-button' title='cancel'>\
+                <img src='/JS-DOM/TODO-List/resources/cancel.png' alt='cancel'></div>" +
+                "<div id='edit-error-message' class='error-message-hidden'>Note can't be empty</div>");
 
             var editInput = newTodoListNote.find(".edit-input");
             editInput.val(newTodoListNoteText);
@@ -57,13 +72,19 @@ $(function () {
             });
 
             saveButton.click(function () {
+                var editErrorMessage = $("#edit-error-message");
+
                 if (editInput.val().length === 0) {
+                    editErrorMessage.removeClass("error-message-hidden");
+                    editErrorMessage.addClass("error-message-visible");
+                    editInput.addClass("invalid");
+
                     return;
                 }
 
                 newTodoListNoteText = editInput.val();
                 setViewMode();
-            })
+            });
         }
     });
 });
