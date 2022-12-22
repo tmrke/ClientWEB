b407a6889778cb78eafd632c47aeee9ca38126e1
Тимур <namat9@yandex.ru>
2022-12-22 15:37:45 +0700

    JS Objects

--- JS/Objects/objects.js
+++ JS/Objects/objects.js
@@ -1,70 +1,82 @@
 (function () {
-    var countries = [{
-        name: "Russia",
-        cities: [{
-            name: "Moscow", population: 13010112
-        }, {
-            name: "Vladivostok", population: 600871
-        }, {
-            name: "Sochi", population: 466078
-        }, {
-            name: "Novosibirsk", population: 1633595
-        }]
-    }, {
-        name: "USA",
-        cities: [{
-            name: "New York", population: 8467513
-        }, {
-            name: "Los Angeles", population: 3898747
-        }, {
-            name: "Vladivostok", population: 675647
-        }]
-    }, {
-        name: "Mexico",
-        cities: [{
-            name: "Mexico", population: 9100000
-        }, {
-            name: "Tijuana", population: 1964788
-        }]
-    }];
+    var countries = [
+        {
+            name: "Russia",
+            cities: [
+                {
+                    name: "Moscow", population: 13010112
+                },
+                {
+                    name: "Vladivostok", population: 600871
+                },
+                {
+                    name: "Sochi", population: 466078
+                },
+                {
+                    name: "Novosibirsk", population: 1633595
+                }
+            ]
+        },
+        {
+            name: "USA",
+            cities: [
+                {
+                    name: "New York", population: 8467513
+                },
+                {
+                    name: "Los Angeles", population: 3898747
+                },
+                {
+                    name: "Boston", population: 675647
+                }
+            ]
+        },
+        {
+            name: "Mexico",
+            cities: [
+                {
+                    name: "Mexico", population: 9100000
+                },
+                {
+                    name: "Tijuana", population: 1964788
+                }
+            ]
+        }
+    ];
 
-    var citiesMaxCountsCountry = getMaxCitiesCountsCountry(countries);
-    console.log(citiesMaxCountsCountry);
+    var citiesMaxCountCountries = getCitiesMaxCountCountries(countries);
+    console.log(citiesMaxCountCountries);
 
     var populationsByCountry = getPopulationsByCountry(countries);
     console.log(populationsByCountry);
-})();
 
-function getMaxCitiesCountsCountry(countries) {
-    if (countries.length === 0) {
-        return null;
+    function getCitiesMaxCountCountries(countries) {
+        if (countries.length === 0) {
+            return [];
+        }
+
+        var citiesMaxCount = countries.reduce(function (citiesCount, country) {
+            return Math.max(citiesCount, country.cities.length);
+        }, 0);
+
+        return countries.filter(function (country) {
+            return country.cities.length === citiesMaxCount;
+        });
     }
 
-    countries.sort(function (country1, country2) {
-        return country2.cities.length - country1.cities.length;
-    });
+    function getPopulationsByCountry(countries) {
+        var populationsByCountry = {};
 
-    return countries.filter(function (country) {
-        return country.cities.length === countries[0].cities.length;
-    });
-}
+        countries.forEach(function (country) {
+            var name = country.name;
+            populationsByCountry[name] = country.cities.reduce(function (sumPopulation, city) {
+                return sumPopulation + city.population;
+            }, 0);
+        });
 
-function getPopulationsByCountry(countries) {
-    var populationsByCountry = {
-        name: [],
-        population: []
-    };
+        return populationsByCountry;
+    }
+})();
 
-    countries.forEach(function (country) {
-        var name = country.name;
-        var population = country.cities.reduce(function (sumPopulation, city) {
-            return sumPopulation + city.population;
-        }, 0)
 
-        populationsByCountry.name.push(name);
-        populationsByCountry.population.push(population);
-    })
-
-    return populationsByCountry;
-}
 
