d45b9a05bc8600b7b665d41705ab0aabccbe583e
Тимур <namat9@yandex.ru>
2022-12-31 18:54:36 +0700

    Bootstrap

--- Vue/TODO-list-vue/todo_list.js
+++ Vue/TODO-list-vue/todo_list.js
@@ -28,9 +28,9 @@ $(function () {
 
         function setViewMode() {
             newTodoListNote.html("<div class='new-todo-list-note row'>\
-            <span class='note-text col-11 col-md-5'></span>\
-            <button class='btn btn-primary edit-button col-5 col-md-1' title='edit'>edit</button>\
-            <button class='btn btn-danger delete-button col-5 col-md-1' title='delete'>delete</button>\
+            <span class='note-text col-11 col-sm-5'></span>\
+            <button class='edit-button col-5 col-sm-1' title='edit'><img src='/JS-DOM/TODO-List/resources/edit.png' alt='edit'></button>\
+            <button class='delete-button col-5 col-sm-1' title='delete'><img src='/JS-DOM/TODO-List/resources/delete.png' alt='delete'></button>\
             </div>");
 
             newTodoListNote.find(".note-text").text(newTodoListNoteText);
@@ -47,9 +47,11 @@ $(function () {
 
         function setEditMode() {
             newTodoListNote.html("<div class='note row'>\
-                <input type='text' class='edit-input is-error-message-hidden col-11 col-md-5' placeholder='Note cant be empty'>\
-                <button class='btn btn-outline-primary save-button col-5 col-md-1' title='save'>save</button>\
-                <button class='btn btn-outline-danger cancel-button col-5 col-md-1' title='cancel'>cancel</button></div>");
+                <input type='text' class='edit-input is-error-message-hidden col-11 col-sm-5' placeholder='Note cant be empty'>\
+                <button class='save-button col-5 col-sm-1' title='save'>\
+                <img src='/JS-DOM/TODO-List/resources/save.png' alt='save'></button>\
+                <button class='cancel-button col-5 col-sm-1' title='cancel'>\
+                <img src='/JS-DOM/TODO-List/resources/cancel.png' alt='cancel'></button></div>");
 
             var editInput = newTodoListNote.find(".edit-input");
             editInput.val(newTodoListNoteText);
