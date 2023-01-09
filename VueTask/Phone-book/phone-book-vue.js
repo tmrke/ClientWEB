452ac8e79745b45dbfcfb5b0ab53a9f9da92c031
Тимур <namat9@yandex.ru>
2023-01-09 23:10:20 +0700

    Phonebook

--- VueTask/Phone-book/phone-book-vue.js
+++ VueTask/Phone-book/phone-book-vue.js
@@ -4,7 +4,6 @@ new Vue({
     data: {
         contacts: [],
         contactId: 1,
-        positionContact: 1,
         name: "",
         lastName: "",
         phone: ""
@@ -12,25 +11,75 @@ new Vue({
 
     methods: {
         addContact: function () {
-            this.contacts.push({
-                name: this.name,
-                lastName: this.lastName,
-                phone: this.phone,
-                id: this.contactId
+            var isCorrect = true;
+            var errorInputs = []
+
+            if (this.name === "") {
+                isCorrect = false;
+            } else {
+                errorInputs.push($("#name-input"))
+            }
+
+            if (this.lastName === "") {
+                isCorrect = false;
+            } else {
+                errorInputs.push($("#last-name-input"))
+            }
+
+            var regex = /^(\+7|7|8)?[\s\-]?\(?[0-9]{3}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
+            var isValid = regex.test(this.phone);
+
+            if (this.phone === "" || !isValid) {
+                errorInputs.push($("#phone-number-input"))
+                isCorrect = false;
+            } else if (this.contacts.phone.indexOf(this.phone) !== -1) {
+                $("#phone-number-input").text("Контакт с таким номером уже существует");
+                isCorrect = false;
+            } else {
+                $("#phone-number-input").text("Номер введен некорректно, введите в формате: +79234567890");
+            }
+
+            if (!isCorrect) {
+                this.showError(errorInputs);
+            } else {
+                this.contacts.push({
+                    name: this.name,
+                    lastName: this.lastName,
+                    phone: this.phone,
+                    id: this.contactId,
+                });
+
+                this.updatePositionContact();
+                this.contactId++;
+                this.name = "";
+                this.lastName = "";
+                this.phone = "";
+            }
+        },
+
+        showError: function (inputs) {
+            inputs.forEach(function (input) {
+                input.addClass("invalid");
             });
-
-            this.contactId++;
-            this.name = "";
-            this.lastName = "";
-            this.phone = "";
         },
 
-        deleteContact: function (contactId){
-
+        deleteContact: function (contactId) {
             this.contacts.splice(contactId, 1);
+            this.updatePositionContact();
         },
 
-        getPositionContact: {
+        updatePositionContact: function () {
+            function updateTableNumeration() {
+                $(".contacts-table tr").each(function (positionNumber) {
+                    $(this).find("td:first").text(positionNumber);
+                });
+            }
+
+            $.ajax({
+                success: function () {
+                    updateTableNumeration();
+                }
+            });
         }
     }
 });
