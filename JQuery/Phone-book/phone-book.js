e6f74bcf4c01b8d0c7b2405216c0d3580d6d16e1
Тимур <namat9@yandex.ru>
2023-01-17 16:14:09 +0700

    Phonebook

--- JQuery/Phone-book/phone-book.js
+++ JQuery/Phone-book/phone-book.js
@@ -2,13 +2,14 @@ $(function () {
     var table = $("#contacts-table");
     var nameInput = $("#name-input");
     var lastNameInput = $("#last-name-input");
-    var phoneNumberInput = $("#phone-number-input");
+    var phoneNumberInput = $("#phone-number-input").mask("+7(999)999-99-99");
     var addButton = $("#add-button");
     var form = $("#form");
     var nameErrorMessage = $("#name-error-message")
     var lastNameErrorMessage = $("#last-name-error-message")
     var phoneErrorMessage = $("#phone-error-message")
     var phonesNumbers = [];
+    var modalDialog = $("#modal-dialog");
 
     form.submit(function (e) {
         e.preventDefault();
@@ -38,7 +39,7 @@ $(function () {
             lastNameErrorMessage.removeClass("invalid");
         }
 
-        var phoneNumber = phoneNumberInput.mask("+7(999)999-99-99").val();
+        var phoneNumber = phoneNumberInput.val();
 
         phoneNumberInput.removeClass("invalid");
         phoneErrorMessage.removeClass("invalid");
@@ -69,46 +70,26 @@ $(function () {
 
         var deleteButton = $("<button class='delete-button' data-bs-toggle='modal' data-bs-target='#modal-dialog'>Удалить</button>");
         var rowsCount = $("#contacts-table tr").length;
-        var modalDialog = $("<div class='modal fade' id='modal-dialog' tabindex='-1' aria-labelledby='modal-dialog-label' aria-hidden='true'>\
-                                 <div class='modal-dialog'>\
-                                     <div class='modal-content'>\
-                                        <div class='modal-header'>\
-                                            <h5 class='modal-title' id='modal-dialog-label'>Вы уверены?</h5>\
-                                        </div>\
-                                        <div class='modal-footer'>\
-                                            <button class='btn bg-danger' data-bs-dismiss='modal' id='yes-button'>Да</button>\
-                                            <button class='btn btn-success' data-bs-dismiss='modal' id='no-button'>Нет</button>\
-                                        </div>\
-                                     </div>\
-                                 </div>\
-                             </div>")
         var newRow = $("<tr></tr>")
             .append($("<td></td>").text(rowsCount))
             .append($("<td></td>").text(nameInputText))
             .append($("<td></td>").text(lastNameInputText))
             .append($("<td></td>").text(phoneNumber))
-            .append(deleteButton).append(modalDialog);
+            .append(deleteButton);
 
         newRow.appendTo(table);
 
-        $("#yes-button").click(function (e) {
-            e.stopPropagation();
-            newRow.remove();
+        deleteButton.click(function () {
+            modalDialog.find(".yes-button").click(function () {
+                newRow.remove()
 
-            function updateTableNumeration() {
                 $(".contacts-table tr").each(function (positionNumber) {
                     $(this).find("td:first").text(positionNumber);
                 });
-            }
 
-            updateTableNumeration();
-            phonesNumbers.splice(phonesNumbers.indexOf(phoneNumber));
+                phonesNumbers.splice(phonesNumbers.indexOf(phoneNumber));
+            });
         });
-
-        deleteButton.click(function (e) {
-            e.stopPropagation();
-        });
-
 
         nameInput.val("");
         lastNameInput.val("");
