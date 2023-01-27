76ff4f98af8406f8f156bd6c904bbabee9d6a5b2
tmrke <namat9@yandex.ru>
2023-01-27 22:32:36 +0700

    Phonebook Vue

--- VueTask/Phone-book/phone-book-vue.js
+++ VueTask/Phone-book/phone-book-vue.js
@@ -7,64 +7,33 @@ new Vue({
         name: "",
         lastName: "",
         phone: "",
-        deletableContactIndex: -1
+        deletableContactIndex: -1,
+        isNameInvalid: false,
+        isLastNameInvalid: false,
+        isPhoneNumberInvalid: false,
+        isContainPhone: false
     },
 
     methods: {
         addContact: function () {
-            var isCorrect = true;
-            var nameInput = document.getElementById("name-input");
-            var nameError = document.getElementById("name-error-message");
-            var lastNameInput = document.getElementById("last-name-input");
-            var lastNameError = document.getElementById("last-name-error-message");
-            var phoneNumberInput = document.getElementById("phone-number-input");
-            var phoneNumberError = document.getElementById("phone-error-message");
-
-            if (this.name === "") {
-                isCorrect = false;
-                nameInput.classList.add("invalid");
-                nameError.style.display = "block";
-            } else {
-                nameInput.classList.remove("invalid");
-                nameError.style.display = "none";
-            }
-
-            if (this.lastName === "") {
-                isCorrect = false;
-                lastNameInput.classList.add("invalid");
-                lastNameError.style.display = "block";
-            } else {
-                lastNameInput.classList.remove("invalid");
-                lastNameError.style.display = "none";
-            }
-
             var regex = /^(\+7|7|8)?[\s\-]?\(?[0-9]{3}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
             var isValid = regex.test(this.phone);
+            this.isContainPhone = false;
+            this.isContainPhone = false;
 
-            var isContainPhone = false;
+            this.isNameInvalid = this.name === "";
+            this.isLastNameInvalid = this.lastName === "";
+            this.isPhoneNumberInvalid = this.isPhoneNumberInvalid === "" || !isValid;
+
+            var self = this;
 
             this.contacts.forEach(function (contact) {
-                if (contact.phone === phoneNumberInput.value) {
-                    isContainPhone = true;
+                if (contact.phone === self.phone) {
+                    self.isContainPhone = true;
                 }
             });
 
-            if (this.phone === "" || !isValid) {
-                phoneNumberInput.classList.add("invalid");
-                phoneNumberError.style.display = "block";
-                isCorrect = false;
-            } else if (isContainPhone) {
-                phoneNumberError.textContent = "Контакт с таким номером уже существует";
-                phoneNumberInput.classList.add("invalid");
-                phoneNumberError.style.display = "block";
-                isCorrect = false;
-            } else {
-                phoneNumberInput.classList.remove("invalid");
-                phoneNumberError.style.display = "none";
-                phoneNumberInput.textContent = "Номер введен некорректно, введите в формате: +79123456789";
-            }
-
-            if (!isCorrect) {
+            if (this.isNameInvalid || this.isLastNameInvalid || this.isPhoneNumberInvalid || this.isContainPhone) {
                 return;
             }
 
@@ -79,14 +48,14 @@ new Vue({
             this.name = "";
             this.lastName = "";
             this.phone = "";
-            isContainPhone = false;
+            this.isContainPhone = false;
         },
 
         deleteContact: function () {
             this.contacts.splice(this.deletableContactIndex, 1);
         },
 
-        putToDeleteContactIndex: function (index) {
+        setDeletableContactIndex: function (index) {
             this.deletableContactIndex = index;
         }
     }
